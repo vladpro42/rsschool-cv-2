330acbe1d90a5a248a4e754c224c90b99e4f70f3
vladpro42 <bondarev.vlad.01@gmail.com>
2022-10-02 23:30:24 +0300

    feat: add responsive 640px

--- online-zoo/pages/main/index.js
+++ online-zoo/pages/main/index.js
@@ -19,3 +19,16 @@ slideBox.forEach( (item, index) => {
         });
     });
 });
+
+
+const testimonialsContent = document.querySelectorAll('.testimonials__content');
+
+testimonialsContent.forEach( (item, index) => {
+    if(index === 0) {
+        item.textContent = 'The best online zoo I’ve met. My son delighted very much ljves to watch gouillas. Online zoo is one jf the ways to instill a love for animals.The best online zoo I’ve met.';
+    } else if( index === 1 ) {
+        item.textContent ='My son delighted very much ljves to watch gouillas. Online zoo is one jf the ways to instill a love for animals. The best online zoo I’ve met. My';
+    } else if( index === 2 ) {
+        item.textContent = 'Online zoo is one jf the ways to instill a love for animals.The best online zoo I’ve met. My';
+    }
+})
