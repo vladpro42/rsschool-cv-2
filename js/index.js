77478680fc73b9ca879c6f754cb3195d5d3193b9
vladpro42 <bondarev.vlad.01@gmail.com>
2023-09-16 10:32:41 +0300

    feat: redaction cv

--- js/index.js
+++ js/index.js
@@ -1,34 +1,34 @@
 const navigationLink = document.querySelectorAll('.navigation__link');
 const navigationBorder = document.querySelectorAll('.navigation__border')
-if( navigationLink ) {
+if (navigationLink) {
 
-    navigationLink.forEach( item => {
+    navigationLink.forEach(item => {
         item.addEventListener('mouseover', () => {
-            navigationBorder.forEach( elem => {
-                if(elem.classList.contains('navigation__border--activ')) {
+            navigationBorder.forEach(elem => {
+                if (elem.classList.contains('navigation__border--activ')) {
                     elem.classList.remove('navigation__border--activ');
                 }
             });
         });
     });
 
-    navigationLink.forEach( (item, indexLink) => {
-        item.addEventListener( 'mouseout', () => {
-            navigationBorder.forEach( (elem, indexBorder) => {
-                if( indexLink === indexBorder ){
+    navigationLink.forEach((item, indexLink) => {
+        item.addEventListener('mouseout', () => {
+            navigationBorder.forEach((elem, indexBorder) => {
+                if (indexLink === indexBorder) {
                     elem.classList.add('navigation__border--activ');
                 }
             })
         });
     });
 
-    
+
 }
 
 let myScroll = document.querySelector('.button-top')
 
-window.addEventListener('scroll', function() {
-    myScroll.classList.toggle('button-top--activ', window.scrollY> 90)
+window.addEventListener('scroll', function () {
+    myScroll.classList.toggle('button-top--activ', window.scrollY > 90)
 });
 
 myScroll.addEventListener('click', () => {
